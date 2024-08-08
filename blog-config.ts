40f811606d4aa67f8ebade42f08bc045763ceab9
GGos3 <dev.ggos3@gmail.com>
2024-08-08 18:00:05 +0900

    chore: 블로그 정보 변경

--- blog-config.ts
+++ blog-config.ts
@@ -1,22 +1,22 @@
 const config = {
-  title: 'Lavender Blog',     // Blog name
-  author: 'Hubert Selby Jr.', // Your name
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  siteUrl: 'https://gatsby-starter-lavender.vercel.app',
+  title: 'GGos3 Blog',     // Blog name
+  author: 'GGos3', // Your name
+  description: 'GGos3의 개발 블로그입니다. 주로 서버와 관련된 내용을 다룹니다.',  // Blog description
+  siteUrl: 'https://blog.ggos3.xyz',
   thumbnail: '/thumbnails/main.png',  // Default blog thumbnail. i.e. '/thumbnails/main.png'
   social: {
-    github: 'blurfx',   // GitHub username
+    github: 'GGos3',   // GitHub username
     twitter: '',        // Twitter username
     facebook: '',       // Facebook username
     linkedin: '',       // LinkedIn username
     instagram: '',      // Instagram username
   },
   comment: {
-    utterances: 'blurfx/gatsby-starter-lavender', // Your GitHub repository
+    utterances: 'GGos3/blog', // Your GitHub repository
     disqusShortName: '',  // Disqus short name
   },
   icon: 'src/images/gatsby-icon.png', // path of blog icon
-  googleAnalytics: '0',  // Google Analytics tracking id
+  googleAnalytics: 'G-0NB4Z5VCEQ',  // Google Analytics tracking id
 };
 
 export default config;
